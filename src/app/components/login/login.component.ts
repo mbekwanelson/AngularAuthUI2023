85abae7f1f9db2d09464f82bf1ff41d69bd0437b
Ntsikelelo.Mbekwa <mbekwantsikelelonelson@gmail.com>
2023-03-13 00:07:37 +0200

    # toggle password visibility feature

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  passwordFieldType : string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa fa-eye-slash"
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  hideOrShowPassword(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa fa-eye" : this.eyeIcon = "fa fa-eye-slash";
+    this.isText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
+
   }
 
 }

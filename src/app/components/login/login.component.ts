7b889aa039297a5c901de70dd3373bd22114fd2c
Ntsikelelo.Mbekwa <mbekwantsikelelonelson@gmail.com>
2023-03-13 01:16:22 +0200

    # Form Validation using class error and custom # validate form function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -32,6 +32,36 @@ export class LoginComponent implements OnInit {
     this.isText ? this.eyeIcon = "fa fa-eye" : this.eyeIcon = "fa fa-eye-slash";
     this.isText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
 
+  }
+
+  onSubmit(){
+
+    if(this.loginForm.valid){
+
+      //send the obj to database
+      console.log(this.loginForm.value);
+
+    }
+    else{
+
+      this.validateAllFormFields(this.loginForm);
+      // throw the error using toaster and with required fields
+    }
+  }
+
+  private validateAllFormFields(formGroup: FormGroup){
+    Object.keys(formGroup.controls).forEach(field=>{
+
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+
+        control.markAsDirty({onlySelf: true});
+      }else if(control instanceof FormGroup){
+
+        this.validateAllFormFields(control);
+      }
+
+    })
   }
 
 }

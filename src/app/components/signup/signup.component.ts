1acfcf703dd45225b880f40734565c6e11615aba
Ntsikelelo.Mbekwa <mbekwantsikelelonelson@gmail.com>
2023-03-13 00:32:48 +0200

    # signup component and scss

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  passwordFieldType : string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  hideOrShowPassword(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
+
   }
 
 }

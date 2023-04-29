f30ebed9998d09f15845405b267cf8b1549ac13a
Ntsikelelo.Mbekwa <mbekwantsikelelonelson@gmail.com>
2023-04-29 23:41:43 +0200

    move validation logic to helper class which is which is reused in login and signup form fields
    - Wrapped up login and signup page ui
    - wrapped up form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import validateForm from 'src/app/helpers/validateForm';
 
 @Component({
   selector: 'app-login',
@@ -29,8 +30,10 @@ export class LoginComponent implements OnInit {
 
   hideOrShowPassword(){
     this.isText = !this.isText;
-    this.isText ? this.eyeIcon = "fa fa-eye" : this.eyeIcon = "fa fa-eye-slash";
-    this.isText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
+    this.isText ?
+     this.eyeIcon = "fa fa-eye" : this.eyeIcon = "fa fa-eye-slash";
+    this.isText ?
+     this.passwordFieldType = "text" : this.passwordFieldType = "password";
 
   }
 
@@ -44,24 +47,11 @@ export class LoginComponent implements OnInit {
     }
     else{
 
-      this.validateAllFormFields(this.loginForm);
+      validateForm.validateAllFormFields(this.loginForm);
       // throw the error using toaster and with required fields
     }
   }
 
-  private validateAllFormFields(formGroup: FormGroup){
-    Object.keys(formGroup.controls).forEach(field=>{
 
-      const control = formGroup.get(field);
-      if(control instanceof FormControl){
-
-        control.markAsDirty({onlySelf: true});
-      }else if(control instanceof FormGroup){
-
-        this.validateAllFormFields(control);
-      }
-
-    })
-  }
 
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import validateForm from 'src/app/helpers/validateForm';
 
 @Component({
   selector: 'app-signup',
@@ -9,16 +11,43 @@ export class SignupComponent implements OnInit {
   passwordFieldType : string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash"
+  signupForm! : FormGroup
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder) {
+
+  }
 
   ngOnInit(): void {
+
+    this.signupForm = this.formbuilder.group({
+      firstname : ["",Validators.required],
+      lastname : ["",Validators.required],
+      email : ["",Validators.required],
+      username : ["",Validators.required],
+      password : ["",Validators.required]
+    });
   }
 
   hideOrShowPassword(){
     this.isText = !this.isText;
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.passwordFieldType = "text" : this.passwordFieldType = "password";
+
+  }
+
+  onSignUp(){
+
+    if(this.signupForm.valid){
+
+      //send the obj to database
+      console.log(this.signupForm.value);
+
+    }
+    else{
+
+      validateForm.validateAllFormFields(this.signupForm);
+      // throw the error using toaster and with required fields
+    }
 
   }
 
